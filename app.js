c590452b0f87227fabce9eb08a1bbbb7e697c9f5
Hedy <duej123456@gmail.com>
2022-11-03 14:54:53 +0800

    新增上方自訂區塊

--- app.js
+++ app.js
@@ -32,15 +32,18 @@ let data = [
     }
 ];
 
-//---------------下方篩選區塊開始------------------
-//元素定義
+//---------------上方自訂區塊開始------------------
+//定義元素
+const submitBtn = document.querySelector(".addTicket-btn");
 const select = document.querySelector(".regionSearch");
 const ticketArea = document.querySelector(".ticketCard-area")
 const cantFindArea = document.querySelector(".cantFind-area")
+const resultNum = document.querySelector("#searchResult-text")
 
-//func
+//func showAllItem
 function showAllItem(){
-    let allItem = ""
+    let allItem = "";
+    let pastPostResult = JSON.parse(localStorage.getItem("pastPost"));
     data.forEach( item => {
         let newItem = `
             <li class="ticketCard">
@@ -75,27 +78,112 @@ function showAllItem(){
             </li>`
         allItem += newItem;
     })
+    if (pastPostResult !== null){
+        pastPostResult.forEach( item => {
+            let newItem = `
+                <li class="ticketCard">
+                    <div class="ticketCard-img">
+                        <a href="#">
+                        <img src=${item.ticketImg}>
+                        </a>
+                        <div class="ticketCard-region"> ${item.ticketRegion
+                        } </div>
+                        <div class="ticketCard-rank"> ${item.ticketRate
+                        } </div>
+                    </div>
+                    <div class="ticketCard-content">
+                        <div>
+                        <h3>
+                            <a href="#" class="ticketCard-name"> ${item.ticketName
+                            } </a>
+                        </h3>
+                        <p class="ticketCard-description">
+                            ${item.ticketDescription}
+                        </p>
+                        </div>
+                        <div class="ticketCard-info">
+                        <div class="ticketCard-num">
+                            <p>
+                            <span><i class="fas fa-exclamation-circle"></i></span>
+                            剩下最後 <span id="ticketCard-num"> ${item.ticketNum
+                            } </span> 組
+                            </p>
+                        </div>
+                        <p class="ticketCard-price">
+                            TWD <span id="ticketCard-price"> ${item.ticketPrice
+                            } </span>
+                        </p>
+                        </div>
+                    </div>
+                </li>`
+            allItem += newItem;
+        })
+    }
     ticketArea.innerHTML = allItem;
+    resultNum.textContent = `本次搜尋共 ${data.length+pastPostResult.length} 筆資料`;
 }
 
+//新增post並渲染頁面
+//取得過去自訂資料
+let pastPost = [];
+submitBtn.addEventListener("click",e => {
+    let ticketName = document.querySelector("#ticketName").value;
+    let ticketImg = document.querySelector("#ticketImgUrl").value;
+    let ticketRegion = document.querySelector("#ticketRegion").value;
+    let ticketPrice = document.querySelector("#ticketPrice").value;
+    let ticketNum = document.querySelector("#ticketNum").value;
+    let ticketRate = document.querySelector("#ticketRate").value;
+    let ticketDescription = document.querySelector("#ticketDescription").value;
 
+
+    let nowPost = {};
+    nowPost.ticketName = ticketName;
+    nowPost.ticketImg = ticketImg;
+    nowPost.ticketRegion = ticketRegion;
+    nowPost.ticketPrice = ticketPrice;
+    nowPost.ticketNum = ticketNum;
+    nowPost.ticketRate = ticketRate;
+    nowPost.ticketDescription = ticketDescription;
+
+    pastPost.push(nowPost)
+    localStorage.setItem("pastPost",JSON.stringify(pastPost));
+    
+    ticketName = "";
+    ticketImg = "";
+    ticketRegion = "";
+    ticketPrice = "";
+    ticketNum = "";
+    ticketRate = "";
+    ticketDescription = "";
+
+    showAllItem()
+})
+//---------------上方自訂區塊結束------------------
+
+//---------------下方篩選區塊開始------------------
 //渲染
+showAllItem()
+
+let pastData = JSON.parse(localStorage.getItem("pastPost"));
+
 select.addEventListener("change",e => {
     const option = e.target.value;
-    console.log(option);
+
     //篩選符合選項的資料
-    let result = data.filter(item => {item.area === option});
-    console.log(result)
+    let result = data.filter(item => item.area === option);
+    let pastPostItem = pastData.filter(item => item.ticketRegion === option)
     //顯示各套票
     if(option === "全部地區"){       
         showAllItem();
-    }else if(result.length === 0){
-        console.log("沒有")
+    }else if(result.length === 0 && pastPostItem.length === 0){
+        ticketArea.innerHTML = "";
         cantFindArea.innerHTML =`
         <h3>查無此關鍵字資料</h3>
         <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/no_found.png?raw=true" alt="">
         `
+        resultNum.textContent = `本次搜尋共 ${result.length} 筆資料`;
     }else{
+        let areaItem = "";
         result.forEach( item => {
                 let newItem = `
                 <li class="ticketCard">
@@ -130,9 +218,44 @@ select.addEventListener("change",e => {
                 </li>`
                 areaItem += newItem;
                 })
-                ticketArea.innerHTML = areaItem
-        }   
+        pastPostItem.forEach( item => {
+            let newItem = `
+            <li class="ticketCard">
+                <div class="ticketCard-img">
+                    <a href="#">
+                    <img src=${item.ticketImg}>
+                    </a>
+                    <div class="ticketCard-region"> ${item.ticketRegion} </div>
+                    <div class="ticketCard-rank"> ${item.ticketRate} </div>
+                </div>
+                <div class="ticketCard-content">
+                    <div>
+                    <h3>
+                        <a href="#" class="ticketCard-name"> ${item.ticketName} </a>
+                    </h3>
+                    <p class="ticketCard-description">
+                        ${item.ticketDescription}
+                    </p>
+                    </div>
+                    <div class="ticketCard-info">
+                    <div class="ticketCard-num">
+                        <p>
+                        <span><i class="fas fa-exclamation-circle"></i></span>
+                        剩下最後 <span id="ticketCard-num"> ${item.ticketNum} </span> 組
+                        </p>
+                    </div>
+                    <p class="ticketCard-price">
+                        TWD <span id="ticketCard-price"> ${item.ticketPrice} </span>
+                    </p>
+                    </div>
+                </div>
+            </li>`
+            areaItem += newItem;
+            })
+                ticketArea.innerHTML = areaItem;
+                cantFindArea.innerHTML ='';
+                resultNum.textContent = `本次搜尋共 ${result.length+pastPostItem.length} 筆資料`;
+    }  
 })
 //---------------下方篩選區塊結束------------------
 
-//---------------上方自訂區塊------------------
